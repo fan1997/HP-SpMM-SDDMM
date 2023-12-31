17175122f6d62069e79e9513bdb49e512344e579
fan1997 <2479031300@qq.com>
2023-12-31 18:58:57 +0800

    update download.sh

--- dataset/download.sh
+++ dataset/download.sh
@@ -1,52 +1,52 @@
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EWC8KQg6QOxEun0ywLQyqvwBdF1OICGqgcPT2AKCQ4dfQw?download=1" -O Yelp.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EWC8KQg6QOxEun0ywLQyqvwBdF1OICGqgcPT2AKCQ4dfQw?download=1" -O Yelp.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ESeVl7v5PldGoYOaEGMytlwB_7UU1CzRLGSRkCFMPdgWMw?download=1" -O Yelp.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EVl41oy2_9pIpBBp62eQMVABYn1GZGJjZSXml17FdBjpNw?download=1" -O Flickr.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EVl41oy2_9pIpBBp62eQMVABYn1GZGJjZSXml17FdBjpNw?download=1" -O Flickr.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ER7PVgU85apKgehIJgtjhqgBZ9_0uaYnMtt-RpSXacw0wQ?download=1" -O Flickr.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EQUyJElzeABCn4hVmXbrT38BAYPmPAA6vui_yYVBz7qmkA?download=1" -O AIFB.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EQUyJElzeABCn4hVmXbrT38BAYPmPAA6vui_yYVBz7qmkA?download=1" -O AIFB.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EbnlKUOlUdBNr0fRNKGnTx4BnldxHaMnK5CJOkE-cHpHeg?download=1" -O AIFB.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EbaLiNIrjTBEmo_EdMKvPJoBRFOZxq7HTiCW_0Oj8IpA0Q?download=1" -O BGS.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EbaLiNIrjTBEmo_EdMKvPJoBRFOZxq7HTiCW_0Oj8IpA0Q?download=1" -O BGS.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ET5mjmjXEclDjpqiHMtb50gBIhDtYTZobnICL3FGLWFwHg?download=1" -O BGS.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ETOSkufmIlhIqyZt4-srwo4BU91IxrRlerFcLFUkSbghkA?download=1" -O AM.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ETOSkufmIlhIqyZt4-srwo4BU91IxrRlerFcLFUkSbghkA?download=1" -O AM.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EcS7pLvaBmtFhL-cst75fngBnalp9TAB4Lx2Am5oBoncRw?download=1" -O AM.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ESEeNrn6YJNNhnlCQ_sZ9uoBYUx-k1SmTqiLIpN-2TZTrw?download=1" -O MUTAG.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ESEeNrn6YJNNhnlCQ_sZ9uoBYUx-k1SmTqiLIpN-2TZTrw?download=1" -O MUTAG.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EfFUf2gwqVZEiGw9rY8BeDIB__DBtg2raEOQXBXzS8jAJQ?download=1" -O MUTAG.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ESUzxbM_rhdHku-Lhogpk9gBKPr25x87IIlgpYOSJj0r2g?download=1" -O AmazonCoBuyComputer.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ESUzxbM_rhdHku-Lhogpk9gBKPr25x87IIlgpYOSJj0r2g?download=1" -O AmazonCoBuyComputer.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/Ech0N_-XIGpJrTvLqkh7CcEBZIzKgKk035qmNwlHS9Sm5g?download=1" -O AmazonCoBuyComputer.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EdKSU_AaKyZCoqtVqY7oAosBGuCwdJtk7audugzMS51gQw?download=1" -O AmazonCoBuyPhoto.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EdKSU_AaKyZCoqtVqY7oAosBGuCwdJtk7audugzMS51gQw?download=1" -O AmazonCoBuyPhoto.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ERh0QpyzotdCt7oNw0172GQB5mQ62jy9FqSR0FW7N8OF-A?download=1" -O AmazonCoBuyPhoto.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EZsX6H38vdZKqLrDkd1_ingBJMKTqBrcTJ_FLB29t2A-Og?download=1" -O CoauthorCS.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EZsX6H38vdZKqLrDkd1_ingBJMKTqBrcTJ_FLB29t2A-Og?download=1" -O CoauthorCS.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EdJPJPrdQ5xJnqzZdtkRYasBt9FJ5saj7DPDJy8A-PH-bQ?download=1" -O CoauthorCS.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ETRqPeQoGgVJsH01HKs0I4AB9UGeybKdw-6l7Em4Hj45JA?download=1" -O CoauthorPhysics.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ETRqPeQoGgVJsH01HKs0I4AB9UGeybKdw-6l7Em4Hj45JA?download=1" -O CoauthorPhysics.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EaDj1lNohy9Auo7V5EzAFlsBT-HubgzeAfoDrNgtSwSw-Q?download=1" -O CoauthorPhysics.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EZuchICOYzJMoZsB28rNe6wB53FOd_Kdqi9P3uhSzkB8Bg?download=1" -O arxiv.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EZuchICOYzJMoZsB28rNe6wB53FOd_Kdqi9P3uhSzkB8Bg?download=1" -O arxiv.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ESdDVwnozz5Lvpk5s8C3V30BxIoFjQkY5s6xAfA5M5bC3A?download=1" -O arxiv.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EWdQbBO62m9Gr2hPtDLoksUBmreBPrCe-27BW6I1a_pRqQ?download=1" -O collab.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EWdQbBO62m9Gr2hPtDLoksUBmreBPrCe-27BW6I1a_pRqQ?download=1" -O collab.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EW-B3l8sHVVElio5h9IsuKUBJXNgBgdT0F0TqVfU1Rx5ow?download=1" -O collab.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EdhzL2jEaFhJrhRXxBkdIYUB_cdYqqvY5V5Ym5lRJ_YaPA?download=1" -O ddi.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EdhzL2jEaFhJrhRXxBkdIYUB_cdYqqvY5V5Ym5lRJ_YaPA?download=1" -O ddi.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EVUCOxAG4VxOm_dlC5AZn-gBqdxjL3AmKLFnYJ-AuKaZJw?download=1" -O ddi.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EdyyI0KDyBlDslTmukGCPCEB7OvXVxI24lpzCOMwU83Vtg?download=1" -O ppa.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EdyyI0KDyBlDslTmukGCPCEB7OvXVxI24lpzCOMwU83Vtg?download=1" -O ppa.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ERcY4BO1gl9IovQO9TmAHuEBPOounoLyayXnbS5jo5b4pg?download=1" -O ppa.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EbmR0Qxr3ShDj_xSW9CVh34ByT6qU8IcHGOza3Z3HZL9Eg?download=1" -O products.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EbmR0Qxr3ShDj_xSW9CVh34ByT6qU8IcHGOza3Z3HZL9Eg?download=1" -O products.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EW1a1DxiyHZKqAFfJv-J05gBgWHBZGW5BLs_PaLQRKQCHA?download=1" -O products.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ES5yBHvmxPVFqn8Rk671Xi4Bdl5RBVSb3FbT_6VtdPXAFA?download=1" -O protein.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/ES5yBHvmxPVFqn8Rk671Xi4Bdl5RBVSb3FbT_6VtdPXAFA?download=1" -O protein.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EQ69AkhFeZ5JknybzebbdpkBXNa2cT1Oxq1WVPn0SPmi_w?download=1" -O protein.cugraph_reorder
 
-#wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EdV5kK-iL7tFkAg0lUeUZA4BgXGqXu-a9jW3PrglyWbFJA?download=1" -O reddit.smtx
+wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EdV5kK-iL7tFkAg0lUeUZA4BgXGqXu-a9jW3PrglyWbFJA?download=1" -O reddit.smtx
 wget "https://hkustgz-my.sharepoint.com/:u:/g/personal/rfan404_connect_hkust-gz_edu_cn/EY66_6BdHzhGvr1N48J1gMIB1AyY_Nnorc8CcsgBO9YEAg?download=1" -O reddit.cugraph_reorder
 
 
